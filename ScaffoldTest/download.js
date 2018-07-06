14940eb9f27720d4f52e45cb95d33ef8f4ccf245
Peter Diltz <zadbr@hotmail.com>
2018-07-06 17:00:05 -0500

    zip download

--- ScaffoldTest/download.js
+++ ScaffoldTest/download.js
@@ -1,17 +1,17 @@
 let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
-	s3.getObject({
+/*	s3.getObject({
 		'Bucket': "zipedits",
 		'Key': event.pathParameters['item']
 	}).promise()
-		.then(data => {
+		.then(data => {*/
 			callback(null,event)
-		})
+	/*	})
 		.catch(err => {
 			callback(err); // an error occurred
 		});
-
+*/
 
 	
 }
