d684511b9f8ea4687cb3616266b239eeaf9c89e4
Peter Diltz <zadbr@hotmail.com>
2018-07-13 14:04:59 -0500

    test sns

--- ScaffoldTest/snslistener.js
+++ ScaffoldTest/snslistener.js
@@ -1,17 +1,28 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-	ddb.put({
-		TableName: 'testlistener',
-		Item: { 'message': event }
-	}, function (err, data) {
-		if (err) {
-			//handle error
-		} else {
-			//your logic goes here
-		}
-	});
+	console.log("Adding DB entry:" + JSON.stringify(event))
 
+
+
+	for (var i = 0; i < event.Records.length; i++) {
+
+
+
+		ddb.put({
+			TableName: 'testlistener',
+			Item: {
+				'testid': Date.now(),
+				'message': event.Records[i].Sns.Message
+			}
+		}, function (err, data) {
+			if (err) {
+				console.log(err)
+			} else {
+				console.log(data)
+			}
+		});
+	}
 
 	callback(null, 'Successfully executed');
 }
