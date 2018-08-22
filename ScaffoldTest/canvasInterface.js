dd16a7706b4dc2252614ea6fb6121306373de519
Peter Diltz <zadbr@hotmail.com>
2018-08-22 10:39:20 -0500

    canvas

--- ScaffoldTest/canvasInterface.js
+++ ScaffoldTest/canvasInterface.js
@@ -1,23 +1,32 @@
 let AWS = require('aws-sdk');
 let request = require('request');
-let url = "http://ec2-18-222-105-76.us-east-2.compute.amazonaws.com/api/v1/";
+let url = "http://ec2-18-222-105-76.us-east-2.compute.amazonaws.com/api/v1";
 let key = "access_token=l9hlJuDTgUY1Q5TygZC7yw7CxZU8tXyJx4Gh93QOxAEI2yihpNt21nZSXujD554R"
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
 	var type = event.type;
 	var suffex = event.suffex;
 	var data = event.data;
 
-	switch(type){
+	switch (type) {
 		case "POST":
-		url+= suffex + "?" + key;
-break;
+			url += suffex + "?" + key;
+			request.post(url,{"form":data}, function (error, response, body) {
+				callback(null, JSON.parse(body));
+			})
+			break;
 		case "PUT":
-		url+= suffex + "?" + key;
-break;
+			url += suffex + "?" + key;
+			request.put(url,{"form":data}, function (error, response, body) {
+				callback(null, JSON.parse(body));
+			})
+			break;
 		default:
-		var da = "&" + data.join("&");
- 		url+= suffex + "?" + key + da;
-		break;
+			var da = "&" + data.join("&");
+			url += suffex + "?" + key + da;
+			request(url, function (error, response, body) {
+				callback(null, JSON.parse(body));
+			})
+			break;
 	}
-	callback(null,url);
+
 }
