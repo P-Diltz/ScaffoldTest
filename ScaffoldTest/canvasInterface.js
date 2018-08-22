0ffa40e8cf0a1ca04eb487a0af0e0207bdaeda7e
Peter Diltz <zadbr@hotmail.com>
2018-08-21 20:10:13 -0500

    canv

--- ScaffoldTest/canvasInterface.js
+++ ScaffoldTest/canvasInterface.js
@@ -1,7 +1,7 @@
 let AWS = require('aws-sdk');
 let request = require('request');
-let url = "";
-let key = ""
+let url = "http://ec2-18-222-105-76.us-east-2.compute.amazonaws.com/api/v1/";
+let key = "access_token=l9hlJuDTgUY1Q5TygZC7yw7CxZU8tXyJx4Gh93QOxAEI2yihpNt21nZSXujD554R"
 exports.handler = function(event, context, callback) {
 	var type = event.type;
 	var suffex = event.suffex;
@@ -10,14 +10,14 @@ exports.handler = function(event, context, callback) {
 	switch(type){
 		case "POST":
 		url+= suffex + "?" + key;
-
+break;
 		case "PUT":
 		url+= suffex + "?" + key;
-
+break;
 		default:
-		url+= suffex + "?" + key + data.join("&");
+		var da = "&" + data.join("&");
+ 		url+= suffex + "?" + key + da;
+		break;
 	}
-
-
 	callback(null,url);
 }
